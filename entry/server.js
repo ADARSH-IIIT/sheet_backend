813752a0cb6a48b8be14982ebce50ab48b876dce
ADARSH-IIIT <adarshtech251@gmail.com>
2024-01-20 00:24:58 +0530

    called connect to db fun in server.js

--- entry/server.js
+++ entry/server.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import connect_to_db from '../database/connect_to_db.js'
 
 const server = express()
 
+
+
+const db_url = process.env.URL || "mongodb://127.0.0.1:27017/google_sheet"
+connect_to_db(db_url)
 
 
 server.get('/' , (req , res)=>{   res.json({mssg : "route working properly"})    })
