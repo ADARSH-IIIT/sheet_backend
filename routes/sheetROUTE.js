fbbe8eb94a9b93af7c41b79823040f13de7e7df1
ADARSH-IIIT <adarshtech251@gmail.com>
2024-01-22 00:25:46 +0530

    smoothness

--- routes/sheetROUTE.js
+++ routes/sheetROUTE.js
@@ -2,6 +2,7 @@ import express from 'express'
 import check_login from '../middleware/check_login.js'
 import { create_sheet, row_wise_access, save_sheet_data } from '../controller/sheetCONTROLLER.js'
 import { check_sheet_admin } from '../middleware/check_sheet_admin.js'
+import { check_row_access } from '../middleware/check_row_access.js'
 
 
 
@@ -14,7 +15,7 @@ sheetROUTER.post( "/create/sheet"  ,   check_login    , create_sheet)
 
 sheetROUTER.post( "/update/row/access/:sheet_id"  ,   check_login ,  check_sheet_admin   , row_wise_access)
 
-sheetROUTER.post( "/save/data/:sheet_id"  ,   check_login   , save_sheet_data)
+sheetROUTER.post( "/save/data/:sheet_id"  ,   check_login , check_row_access  , save_sheet_data)
 
 
 
